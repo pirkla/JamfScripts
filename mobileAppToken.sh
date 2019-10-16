b1af77f131eaaff351b8f2b01af9f1f25a3f286c
Andrew Pirkl <andrew.pirkl@gmail.com>
2019-10-16 10:47:48 -0500

    Add files via upload

---
+++ mobileAppToken.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+#use  the format https://your.url:8443/JSSResource if served on port 8443
+jssURL="https://yoururl.jamfcloud.com/JSSResource"
+jssUser="admin"
+jssPass="password"
+newToken="999"
+oldToken="999"
+
+# get the ids from the endpoint
+ids=$(curl -H "Content-Type: application/xml" -ksu "$jssUser":"$jssPass" "${jssURL}/mobiledeviceapplications" -X GET | xpath "//id[not(ancestor::site)]" 2> /dev/null | sed s/'<id>'//g | sed s/'<\/id>'/','/g)
+# parse those ids into an array
+IFS=', ' read -r -a allIDs <<< ${ids}
+
+# loop over each id
+for curID in ${allIDs[@]};
+do
+    echo "checking $curID"
+    # get the VPP xml subset
+    vpp=$(curl -H "Accept : text/xml" -H "Content-Type: text/xml" -ksu "$jssUser":"$jssPass" "$jssURL/mobiledeviceapplications/id/$curID/subset/vpp" -X GET)
+    # parse that into the token the current id is using
+    token=$(echo $vpp | xpath //vpp/vpp_admin_account_id/text\(\) 2> /dev/null)
+    
+    # if the old token is being used then switch tokens
+	if [ "$token" == "$oldToken" ]; then
+		    update=$(curl -H "Accept : text/xml" -H "Content-Type: text/xml" -ksu "$jssUser":"$jssPass" "$jssURL/mobiledeviceapplications/id/$curID" -X PUT -d  "<mobile_device_application>
+                <vpp>
+                    <vpp_admin_account_id>$newToken</vpp_admin_account_id>
+                </vpp>
+            </mobile_device_application>")
+    echo "******* updated $curID"
+	fi
+done
