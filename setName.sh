4a22b5a940eabcf7aeb3ba265a2695c7689af0e3
Andrew Pirkl <andrew.pirkl@gmail.com>
2019-11-14 15:47:45 -0600

    add set name script

---
+++ setName.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# set the computer name, host name, and local host name of a computer to string + serial + string
+
+prepend=$4
+append=$5
+callEvent=$6
+
+serial=$(system_profiler SPHardwareDataType | awk '/Serial/ {print $4}')
+compName="$prepend""$serial""$append"
+
+echo "setting computer name:""$compName"
+scutil --set ComputerName "$compName"
+scutil --set HostName "$compName"
+scutil --set LocalHostName "$compName"
